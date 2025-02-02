4ba35a36c04f934eaac8a19e3ddd7e416cd92100
Pri <119393765+developer-priya-mishra@users.noreply.github.com>
2025-02-02 16:38:08 +0530

    "JSX comes"

--- React/App.js
+++ React/App.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const heading = React.createElement('h1',
-    {id:'heading',xyz:"abc"},
-    "Hello from React!"
-)
-
+const jsxHeading = <h1 className="heading">Hello from JSX! </h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading);
+
+root.render(jsxHeading);
