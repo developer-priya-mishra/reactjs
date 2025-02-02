07de8bddf6b0d5ae76d1967aaaa55528dc887e3a
Pri <119393765+developer-priya-mishra@users.noreply.github.com>
2025-02-02 20:21:14 +0530

    "Fuctional Component"

--- React/App.js
+++ React/App.js
@@ -3,6 +3,25 @@ import ReactDOM  from "react-dom/client";
 
 const jsxHeading = <h1 className="heading">Hello from JSX! </h1>
 
+const Heading1 = () => {
+    return <h1 className="heading">Hello from React Functional Component Heading1 </h1>
+}
+
+const Heading2 = () => <h1 className="heading">Hello from React Functional Component Heading2</h1>
+
+const Heading3 = () => (<h1 className="heading">Hello from React Functional Component Heading3</h1>)
+
+const Headings = () =>{
+    return (
+        <>
+        <Heading1/>
+        <Heading2/>
+        <Heading3/>
+        <h1 className="heading">Hello from React Functional Component Headings</h1>
+    </>
+   )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(jsxHeading);
+root.render(<Headings/>);
